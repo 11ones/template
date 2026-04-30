988df080caab50569da678d220431c685f424503
11one <54321437+11ones@users.noreply.github.com>
2026-05-01 02:22:48 +0900

    segment tree operations
    Replaced commented-out main function with an updated version that includes input handling and segment tree operations.

--- seg.cpp
+++ seg.cpp
@@ -24,7 +24,7 @@ array<int, 2> F(int p, int x, int y, int s, int e) {
   array<int, 2> r = F(p << 1 | 1, x, y, (s + e) / 2 + 1, e);
   return {min(l[0], r[0]), max(l[1], r[1])};
 }
-
+/*
 int main() {
   cin.tie(0)->sync_with_stdio(0);
   int N, M, x, y;
@@ -37,5 +37,30 @@ int main() {
     cin >> x >> y;
     auto t = F(1, --x, --y, 0, N - 1);
     cout << t[0] << " " << t[1] << "\n";
+  }
+}
+*/
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  const long MOD = 1'000'000'007;
+  long N, M, K, x, y, z;
+  cin >> N >> M >> K;
+  M += K;
+  vector<long> v(2 * N, 1);
+  for (int i = 0; i < N; ++i) cin >> v[N + i];
+  for (int i = N - 1; i > 1; --i) v[i] = v[i << 1] * v[i << 1 | 1] % MOD;
+  while (M--) {
+    cin >> x >> y >> z;
+    if (x == 1) {
+      for (v[y += N - 1] = z; y > 1; y >>= 1) v[y >> 1] = v[y] * v[y ^ 1] % MOD;
+    } else {
+      x = 1;
+      for (y += N - 1, z += N; y < z; y >>= 1, z >>= 1) {
+        if (y & 1) x = x * v[y++] % MOD;
+        if (z & 1) x = x * v[--z] % MOD;
+      }
+      cout << x << '\n';
+    }
   }
 }

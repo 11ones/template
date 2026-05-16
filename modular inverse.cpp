26aaaf9f4642da2927f06bb4037d3cebb5349a52
11one <joseunggeum@gmail.com>
2026-05-16 22:04:40 +0900

    edit

--- modular inverse.cpp
+++ modular inverse.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const long MOD = 998244353;
+
+long power(long a, long b) {
+  long res = 1, t = 1;
+  a %= MOD;
+  do {
+    if (b & t) res = (res * a) % MOD;
+    a = (a * a) % MOD;
+  } while (b >= (t <<= 1));
+  return res;
+}
+
+long mi(long n) { return power(n, MOD - 2); }
+
+
+/*
 // 분할 정복을 이용한 거듭제곱 (a^b % mod)
 long long power(long long a, long long b, long long mod) {
     long long res = 1, t = 1;
@@ -16,10 +32,11 @@ long long power(long long a, long long b, long long mod) {
 long long modInverse(long long n, long long mod) {
     return power(n, mod - 2, mod);
 }
+*/
 
 int main() {
     const long MOD = 998244353;
     long a, b;
     cin >> a >> b;
-    cout << a * modInverse(b, MOD);
+    cout << a * mi(b);
 }
